c43b21aeb6e08804265ac3222ae5c6e593f90373
nguyenvukhang <brew4k@gmail.com>
2024-06-17 13:35:03 +0800

    Load theorems into memory

--- Makefile
+++ Makefile
@@ -1,3 +1,4 @@
 run:
 	cargo build
+	rm -f ~/.local/state/nvim/lsp.log
 	cd /home/khang/repos/math && nvim lib/real_analysis/divergence.tex
--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,109 @@
-use std::fs;
+use std::collections::HashMap;
 use std::io::Write;
+use std::path::{Path, PathBuf};
+use std::{fs, io};
 
 use anyhow::Result;
 use lsp_server::{Connection, Message, Response};
 use lsp_types::{
     ClientCapabilities, GotoDefinitionResponse, InitializeParams,
-    ServerCapabilities,
+    ServerCapabilities, WorkspaceFolder,
 };
 
-fn log<S: AsRef<str>>(x: S) {
-    let mut f = fs::File::options()
-        .append(true)
-        .open("/home/khang/repos/mylsp/log.txt")
-        .unwrap();
-    writeln!(f, "{}", x.as_ref()).unwrap();
+fn handle_message(connection: &Connection, msg: Message) -> Result<()> {
+    match msg {
+        Message::Request(req) => {
+            eprintln!("REQUEST: {req:?}");
+            if connection.handle_shutdown(&req)? {
+                return Ok(());
+            }
+            match req.method.as_str() {
+                "textDocument/definition" => {
+                    let resp = Response {
+                        id: req.id,
+                        result: Some(
+                            serde_json::to_value(
+                                GotoDefinitionResponse::Array(vec![]),
+                            )
+                            .unwrap(),
+                        ),
+                        error: None,
+                    };
+                    connection.sender.send(Message::Response(resp))?;
+                }
+                _ => (),
+            }
+        }
+        Message::Response(resp) => {
+            eprintln!("RESPONSE: {resp:?}");
+        }
+        Message::Notification(not) => {
+            eprintln!("NOTIFICATION: {not:?}");
+        }
+    }
+    Ok(())
+}
+
+#[derive(Debug, Default)]
+struct State {
+    files: HashMap<PathBuf, Vec<String>>,
+}
+
+impl State {
+    fn load_folder_recursively<P: AsRef<Path>>(
+        &mut self,
+        root: P,
+    ) -> io::Result<()> {
+        for entry in
+            walkdir::WalkDir::new(root).into_iter().filter_map(|e| e.ok())
+        {
+            if entry.metadata().map_or(false, |v| v.is_file()) {
+                if entry.path().extension().map_or(false, |v| v == "tex") {
+                    self.update_file(entry.path())?;
+                }
+            }
+        }
+        Ok(())
+    }
+
+    fn update_file<P: AsRef<Path>>(&mut self, path: P) -> io::Result<()> {
+        let mut lines = vec![];
+        if let Ok(text) = fs::read_to_string(path.as_ref()) {
+            for line in text.lines() {
+                if line.starts_with("\\Theorem") {
+                    lines.push(line.to_string());
+                }
+            }
+        }
+        self.files.insert(path.as_ref().to_path_buf(), lines);
+        Ok(())
+    }
+
+    fn print_lines(&self) {
+        for lines in self.files.values() {
+            for line in lines {
+                eprintln!("->{line}");
+            }
+        }
+    }
 }
 
 fn actual_main() -> Result<()> {
+    let mut state = State::default();
     let (connection, io_threads) = Connection::stdio();
     let (id, params) = connection.initialize_start()?;
     let init_params: InitializeParams = serde_json::from_value(params).unwrap();
+    // eprintln!("INIT PARAMS {:?}", init_params);
+    if let Some(workspace_folders) = init_params.workspace_folders {
+        for wsf in workspace_folders {
+            state.load_folder_recursively(wsf.uri.path().as_str())?;
+        }
+    }
+    eprintln!("LINES");
+    state.print_lines();
+
     let client_capabilities: ClientCapabilities = init_params.capabilities;
+    // eprintln!("CLIENT CAPABILITIES {:?}", client_capabilities);
 
     let mut server_capabilities = ServerCapabilities::default();
     server_capabilities.definition_provider =
@@ -37,53 +120,7 @@ fn actual_main() -> Result<()> {
     connection.initialize_finish(id, initialize_data)?;
 
     for msg in &connection.receiver {
-        eprintln!("got msg: {msg:?}");
-        match msg {
-            Message::Request(req) => {
-                if connection.handle_shutdown(&req)? {
-                    return Ok(());
-                }
-                if req.method == "textDocument/definition" {
-                    log("asked for def");
-                    let resp = Response {
-                        id: req.id,
-                        result: Some(
-                            serde_json::to_value(
-                                GotoDefinitionResponse::Array(vec![]),
-                            )
-                            .unwrap(),
-                        ),
-                        error: None,
-                    };
-                    connection.sender.send(Message::Response(resp))?;
-                }
-                // let x: GotoDefinition;
-                // eprintln!("got request: {req:?}");
-                // match cast::<GotoDefinition>(req) {
-                //     Ok((id, params)) => {
-                //         eprintln!("got gotoDefinition request #{id}: {params:?}");
-                //         let result = Some(GotoDefinitionResponse::Array(Vec::new()));
-                //         let result = serde_json::to_value(&result).unwrap();
-                //         let resp = Response {
-                //             id,
-                //             result: Some(result),
-                //             error: None,
-                //         };
-                //         connection.sender.send(Message::Response(resp))?;
-                //         continue;
-                //     }
-                //     Err(err @ ExtractError::JsonError { .. }) => panic!("{err:?}"),
-                //     Err(ExtractError::MethodMismatch(req)) => req,
-                // };
-                // ...
-            }
-            Message::Response(resp) => {
-                eprintln!("got response: {resp:?}");
-            }
-            Message::Notification(not) => {
-                eprintln!("got notification: {not:?}");
-            }
-        }
+        handle_message(&connection, msg)?;
     }
     Ok(())
 }
